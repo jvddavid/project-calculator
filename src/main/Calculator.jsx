8f94e85e0e1202b30c0ce1247d188cabab599f42
João Victor Davi <j.victordavid2@gmail.com>
2022-04-27 15:32:16 -0300

    feat: create initial version of Calculator component

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,17 @@
+/*
+Author: João Victor David de Oliveira (j.victordavid2@gmail.com)
+Calculator.jsx (c) 2022
+Desc: file to create the calculator component
+Created:  2022-04-27T17:57:20.042Z
+Modified: 2022-04-27T18:00:20.214Z
+*/
+
+import React, { Component } from "react";
+
+import "./Calculator.css";
+
+export default class Calculator extends Component {
+  render() {
+    return <div className="calculator"></div>;
+  }
+}
